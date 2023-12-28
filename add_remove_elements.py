b4cd5f909f1e48af0b635f542be2ac6c76ac453a
deepakstomar <deepaktomar.tech@gmail.com>
2023-12-28 15:06:50 +0530

    Add/Remove elements done

--- add_remove_elements.py
+++ add_remove_elements.py
@@ -1,8 +1,8 @@
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
 
 TARGET_WEB_PAGE = 'https://the-internet.herokuapp.com/'
 CHROME_PATH = '/home/my/Downloads/chromedriver-linux64/chromedriver'
@@ -11,3 +11,26 @@ service = Service(CHROME_PATH)
 
 driver = webdriver.Chrome(service=service)
 driver.get(TARGET_WEB_PAGE)
+action = ActionChains(driver)
+
+link = driver.find_element(By.XPATH, "//a[normalize-space()='Add/Remove Elements']")
+link.click()
+
+add_button = driver.find_element(By.XPATH, "//button[@onclick='addElement()']")
+# add_button.click()
+action.click(on_element=add_button)
+action.click(on_element=add_button)
+action.click(on_element=add_button)
+action.click(on_element=add_button)
+action.click(on_element=add_button)
+action.perform()
+
+# time.sleep(5)
+delete_buttons = driver.find_elements(By.XPATH, "//button[@class='added-manually']")
+
+for delete_button in delete_buttons:
+    delete_button.click()
+
+# time.sleep(5)
+
+driver.quit()
